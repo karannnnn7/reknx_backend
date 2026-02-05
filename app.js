f076dad974959bc905ca74cd1376c154658048b2
Karan Kapdi <karannnofficial7@gmail.com>
2026-02-05 12:49:59 +0530

    bug fix

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ import cookieParser from "cookie-parser";
 const app = express()
 var corsOptions = {
   origin: process.env.WHITE_LIST,
-    credentials: true,
+  credentials: true,
 }
 app.use(cors(corsOptions))
 
 app.use(express.json())
-app.use(express.urlencoded({extended: true,limit: "100kb"}))
+app.use(express.urlencoded({ extended: true, limit: "100kb" }))
 app.use(express.static("public"))
 app.use(cookieParser())
 
 
-app.get('/',(req,res)=> {
-    res.send('server is running ....')
+app.get('/', (req, res) => {
+  res.send('server is running ....')
 })
 
 // routes
@@ -25,7 +25,7 @@ import userRouter from "./routes/user.routes.js";
 app.use("/api/v1/users", userRouter)
 
 import portfolioRouter from './routes/portfollio.routes.js';
-app.use('/api/v2/portfolio',portfolioRouter)
+app.use('/api/v2/portfolio', portfolioRouter)
 
 
-export { app }
+export default app
