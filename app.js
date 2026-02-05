eead1ca7a71001c8d3f49ae74e42c6db62f6c4d1
Karan Kapdi <karannnofficial7@gmail.com>
2026-02-05 14:03:49 +0530

    8

--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import cookieParser from "cookie-parser";
+import connectDB from "./db/db.js";
+
+await connectDB()
 
 const app = express()
 var corsOptions = {
--- index.js
+++ index.js
@@ -2,20 +2,10 @@ import dotenv from "dotenv"
 dotenv.config({
     path: '.env'
 })
-import connectDB from "./db/db.js";
-import { app } from './app.js'
+import app from './app.js'
 
 
-connectDB()
-// .then(() => {
-//     app.on("error", (error) => {
-//             console.log("ERRR: ", error);
-//             throw error
-//         })
-//     app.listen(process.env.PORT || 8000, () => {
-//         console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
-//     })
-// })
-// .catch((err) => {
-//     console.log("MONGO db connection failed !!! ", err);
-// })
+
+app.listen(process.env.PORT || 8000, () => {
+    console.log(`⚙️ Server is running at port : ${process.env.PORT}`);
+})
